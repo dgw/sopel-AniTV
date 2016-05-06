49ae3abaffee366b6913096b28927dd21dce6094
dgw <dgw@technobabbl.es>
2016-05-06 03:25:58 -0500

    Handle non-JSON errors from the API

--- anitv.py
+++ anitv.py
@@ -50,7 +50,11 @@ def anitv(bot, trigger):
     except requests.exceptions.HTTPError as e:
         bot.say("HTTP error: " + e.message)
         return
-    data = r.json()
+    try:
+        data = r.json()
+    except ValueError:
+        bot.say(r.content)
+        return
     queue = []
     for result in data['results']:
         if 'error' in result:
